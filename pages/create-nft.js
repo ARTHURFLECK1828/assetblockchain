7fa128da43fc6b3ddbf3a16bc19ca727ce556a4f
Aravind <70009354+ARTHURFLECK1828@users.noreply.github.com>
2022-05-16 14:40:00 +0530

    Add files via upload

---
+++ pages/create-nft.js
@@ -0,0 +1,221 @@
+/* pages/create-nft.js */
+import { useState } from 'react'
+import { ethers } from 'ethers'
+import { create as ipfsHttpClient } from 'ipfs-http-client'
+import { useRouter } from 'next/router'
+import Web3Modal from 'web3modal'
+
+import crustAuth from '../scripts/crustAuth';
+import crustPinning from '../scripts/crustPinning';
+import placeStorageOrder from '../scripts/placeStorageOrder';
+
+import { css } from '@emotion/css'
+
+const authHeader = crustAuth();
+
+const client = ipfsHttpClient({
+  // url:'https://ipfs.infura.io:5001/api/v0',
+  url: 'https://crustipfs.xyz',
+  headers: {
+    authorization: `Basic ${authHeader}`
+  }
+});
+
+import {
+  marketplaceAddress
+} from '../config'
+
+import NFTMarketplace from '../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json'
+// import { url } from 'inspector'
+
+export default function CreateItem() {
+  const [fileUrl, setFileUrl] = useState(null)
+  const [formInput, updateFormInput] = useState({ price: '', name: '', description: '' })
+  const [fileCid, setFileCid] = useState(null)
+  const [fileSize, setFileSize] = useState(null)
+  const [coverImageUrl, setCoverImageUrl] = useState(null)
+  const router = useRouter()
+
+  async function onChange(e) {
+    /* upload image to IPFS */
+    const file = e.target.files[0]
+    try {
+      const added = await client.add(
+        file,
+        {
+          progress: (prog) => console.log(`received: ${prog}`)
+        }
+      )
+      const cid = added.cid;
+      setFileCid(cid);
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`
+      setFileUrl(url)
+
+      console.log("------------FILESTAT-------------");
+      const fileStat = await client.files.stat("/ipfs/" + added.path);
+      console.log(fileStat);
+      setFileSize(fileStat.cumulativeSize);
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }  
+  }
+
+  async function onChangeCoverImage(e) {
+    /* upload image to IPFS */
+    const file = e.target.files[0]
+    try {
+      const added = await client.add(
+        file,
+        {
+          progress: (prog) => console.log(`received: ${prog}`)
+        }
+      )
+      const cid = added.cid;
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`
+      setCoverImageUrl(url)
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }  
+  }
+
+  async function uploadToIPFS() {
+    const { name, description, price } = formInput
+    if (!name || !description || !price || !fileUrl || !fileCid) return
+    /* first, upload metadata to IPFS */
+    const data = JSON.stringify({
+      name, description, file: fileUrl, image: coverImageUrl
+    })
+    try {
+      await crustPinning(fileCid, name);
+      await placeStorageOrder(fileCid, fileSize);
+      const added = await client.add(data)
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`
+      /* after metadata is uploaded to IPFS, return the URL to use it in the transaction */
+      return url
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }  
+  }
+
+  async function listNFTForSale() {
+    const url = await uploadToIPFS()
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+
+    /* create the NFT */
+    const price = ethers.utils.parseUnits(formInput.price, 'ether')
+    let contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, signer)
+    let listingPrice = await contract.getListingPrice()
+    listingPrice = listingPrice.toString()
+    let transaction = await contract.createToken(url, price, { value: listingPrice })
+    await transaction.wait()
+
+    router.push('/')
+  }
+
+  return (
+    <div className={sellPage}>
+      <div className={sellPageContainer}>
+      To add your Asset fill in the following details
+        <p className={titles}>Enter Name of Asset to be Sold:</p>
+        <input 
+          placeholder="Asset Name"
+          className={sellPageInput}
+          onChange={e => updateFormInput({ ...formInput, name: e.target.value })}
+          style={{color:'grey'}}/>
+        <p className={titles}>Enter a short Description of the Asset:</p>
+        <textarea
+          placeholder="Asset Description"
+          className={sellPageInput}
+          onChange={e => updateFormInput({ ...formInput, description: e.target.value })}
+          style={{color:'grey'}}/>
+        <p className={titles}>Enter the Price of the Asset in ETH:</p>
+        <input
+          placeholder="Asset Price in Eth"
+          className={sellPageInput}
+          onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
+        style={{color:'grey'}}/>
+        <p className={titles}>Upload Cover Image:</p>
+        <input
+          type="file"
+          name="Asset"
+          className={sellPageInput}
+          onChange={onChangeCoverImage}
+          style={{color: 'grey'}}
+          />
+          {
+            coverImageUrl && (<img className={uploadedImg} width="350" src={coverImageUrl} />)
+          }
+        <p className={titles}>Upload Document:</p>
+        <input
+          type="file"
+          name="Asset"
+          className={sellPageInput}
+          onChange={onChange}
+          style={{color: 'grey'}}
+          onClick={() => document.getElementById("loading").style.display = "block"}
+        />
+        <p className={loading} id='loading'>Loading....</p>
+        {
+          fileUrl && (document.getElementById('loading').innerHTML="")
+        }
+        <button onClick={listNFTForSale} className={PostDoc}>
+          Sell
+        </button>
+      </div>
+    </div>
+  )
+}
+const titles=css`
+display: flex;
+justify-content:left;
+align-items: left;
+flex-direction: column;
+width: 100%;
+color:white;
+`
+const sellPage = css`
+    padding-top: 160px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+`
+const sellPageContainer = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 60%;
+    font-size:25px;
+    color:white;
+`
+const sellPageInput = css`
+    width: 100%;
+    margin: 10px 0px;
+    padding: 10px 10px;
+    border-radius: 10px;
+    background-color:#1b305c;
+`
+const loading = css`
+    display: none;
+    color: white;
+`
+const uploadedImg = css`
+    
+`
+const PostDoc = css`
+    margin: 20px 0px;
+    padding: 10px 20px;
+    border-radius: 5px;
+    background-color: #046EFC;
+    color:white;
+    font-weight: 600;
+    transition: all 0.3s ease-in-out;
+    :hover {
+      border-radius: 15px;
+      background-color:#AA6EC8;
+    }
+`
---
+++ pages/index.js
@@ -0,0 +1,169 @@
+/* pages/index.js */
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Web3Modal from 'web3modal'
+import { css } from '@emotion/css'
+import Image from 'next/image'
+
+import {
+  marketplaceAddress
+} from '../config'
+import Link from 'next/link';
+import NFTMarketplace from '../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json'
+
+export default function Home() {
+  const [nfts, setNfts] = useState([])
+  const [loadingState, setLoadingState] = useState('not-loaded')
+  useEffect(() => {
+    loadNFTs()
+  }, [])
+  async function loadNFTs() {
+    /* create a generic provider and query for unsold market items */
+    const provider = new ethers.providers.JsonRpcProvider()
+    const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, provider)
+    const data = await contract.fetchMarketItems()
+
+    /*
+    *  map over items returned from smart contract and format 
+    *  them as well as fetch their token metadata
+    */
+    const items = await Promise.all(data.map(async i => {
+      const tokenUri = await contract.tokenURI(i.tokenId)
+      const meta = await axios.get(tokenUri)
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+      let item = {
+        price,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+        name: meta.data.name,
+        description: meta.data.description,
+      }
+      return item
+    }))
+    setNfts(items)
+    setLoadingState('loaded')
+  }
+  async function buyNft(nft) {
+    /* needs the user to sign the transaction, so will use Web3Provider and sign it */
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, signer)
+
+    /* user will be prompted to pay the asking proces to complete the transaction */
+    const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')
+    const transaction = await contract.createMarketSale(nft.tokenId, {
+      value: price
+    })
+    await transaction.wait()
+    loadNFTs()
+  }
+
+  if (loadingState === 'loaded' && !nfts.length) return (<h1 className={noNftsHome}>No items in marketplace</h1>)
+  return (
+    <div className={home}>
+      Recently added Assets :
+      <div className={displayNft}>
+        <div className={displayNftContainer}>
+          {
+            nfts.map((nft, i) => (
+
+              <div key={i} className={nftBox}>
+                <p className={nftSeller}>Creator: {nft.seller}</p>
+                <img className={nftImg} src={nft.image} />
+                {/* <p className="">Seller: {nft.seller}</p> */}
+                <p className={nftBoxName}>{nft.name}</p>
+                {/* <p className={nftBoxDes}>{nft.description}</p> */}
+                <p className={nftBoxPrc}>{nft.price} ETH&nbsp;&nbsp;&nbsp;<Image width={30} height={30} src={require('./assets/etherium.svg')} /></p>
+                <button className={buyNftBtn} onClick={() => buyNft(nft)}>Buy Now</button>
+                <Link
+                  href={{
+                    pathname: "/display-nft",
+                    query: {
+                      img: nft.image,
+                      name: nft.name,
+                      description: nft.description,
+                      price: nft.price,
+                      seller: nft.seller,
+                    }
+                  }} style={{color:'green'}}>View More</Link>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+const noNftsHome = css`
+    color: white;
+    padding-top: 140px;
+    text-align: center;
+    font-size: 30px;
+`
+const home = css`
+    padding-top: 140px;
+    color: orange;
+    padding-left: 300px;
+    font-size:30px;
+    `
+const displayNft = css`
+    width: 100%;
+`
+const displayNftContainer = css`
+    display: flex;
+    justify-contents: center;
+    align-items: center;
+    flex-wrap: wrap;
+`
+const nftBox = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 300px;
+  padding: 20px 5px;
+  margin: 15px;
+  border: 1px solid rgba(201, 22, 220, 0.39);
+  border-radius: 10px;
+  color: black;
+  font-size:20px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    transform:scale(1.05);
+    transition-duration: 0.5s;
+  }
+`
+const nftSeller = css`
+    font-size: 10px;
+`
+
+const nftBoxName = css`
+  font-size: 30px;
+  font-weight: 600;
+`
+const nftBoxPrc = css`
+  margin-top: 10px;
+  color: #ff7324;
+  display: flex;
+  align-items: center;
+`
+const buyNftBtn = css`
+  margin-top: 20px;
+  background:#AA6EC8;
+  padding: 5px 15px;
+  border: 2px solid #111;
+  border-radius: 10px;
+  margin-bottom: 20px;
+`
+const nftImg = css`
+    width: 95%;
+    border-radius: 10px;
+    margin-top: 20px;
+    margin-bottom: 10px;
+`
